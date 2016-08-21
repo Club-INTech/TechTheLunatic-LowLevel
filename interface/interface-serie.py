d74612f68c43e4de09c2bf1e52953f2ef807d1ae
BIG Boss <remi.dulong@telecom-sudparis.eu>
2016-08-21 19:26:58 +0200

    debut de l'interface graphique de test LL

--- interface/interface-serie.py
+++ interface/interface-serie.py
@@ -1,141 +1,91 @@
 from serial import Serial, SerialTimeoutException, SerialException
-from asserv2 import *
-import matplotlib.pyplot as plt
-import time
+from Tkinter import *
+from threadEcoute import *
 
 
-class serialCom:
-    def __init__(self, serialPort):
-        self.port = serialPort
-        try:
-            self.port_serie = Serial(port=self.port, baudrate=115200, timeout=0)
-            print self.port_serie.isOpen()
-        except SerialException:
-            print "impossible d'ouvrir le port : " + str(serialPort)
-            raw_input()
-            exit()
+fenetre = Tk()
 
-    def ping(self):
-        
-        self.port_serie.write("?\r\n")
-          
-    def ecouter(self):
-        a=self.port_serie.readline() 
-        #print a
-        return a
-    def attendre(self):
-        return self.port_serie.inWaiting()
+label = Label(fenetre, text="INTerface")
+label.pack()
 
-    def parler(self, a_envoyer):
-        #raw_input("écrire ici")
-        self.port_serie.write(a_envoyer+"\r\n")
-        return a_envoyer
+gauche = PanedWindow(fenetre, orient=VERTICAL)
+gauche.pack(side=LEFT, expand="yes", fill=BOTH, pady=2, padx=2)
 
-    def quitter(self):
-        self.port_serie.close()
+droite = PanedWindow(fenetre, orient=VERTICAL)
+droite.pack(side=RIGHT, expand="yes", fill=BOTH, pady=2, padx=2)
 
-com=serialCom("COM6")
-print("--------------------------\n\n")
-m=""
-a=""
+state = LabelFrame(gauche, text="Etat du robot", padx=20, pady=20)
+state.pack(fill="both", expand="no", side=TOP)
 
-while(m!='exit'):
-    
-    m=raw_input("ordre : ")
-    serialCom.parler(com, m)
-    if(m=="testSpeed"):
+Label(state, text="Position :").pack()
+Label(state, text="Orientation :").pack()
+Label(state, text="Ultra Sons :").pack()
 
-        speedG=[]
-        speedD=[]
-        VcD=[]
-        VcG=[]
-        pwmD=[]
-        pwmG=[]
-        
-        time.sleep(5)
-        
-        while(serialCom.attendre(com)>0):
-            a=serialCom.ecouter(com)
-            data = a.rsplit("\t")
-            if len(data) == 9:
-                speedG.append(np.abs(float(data[2])))
-                speedD.append(np.abs(float(data[3])))
-                VcG.append(np.abs(float(data[4])))
-                VcD.append(np.abs(float(data[5])))
-                pwmG.append(float(data[6]))
-                pwmD.append(float(data[7]))
-                
-        T= np.linspace(0, 1.5, len(speedG))
-        plt.plot(T, speedG, 'r')
-        plt.plot(T, speedD, 'k')
-        plt.plot(T, VcG, 'b')
-        
-        plt.autoscale()
-        plt.show()
-        plt.plot(T, pwmG, 'g')
-        plt.show()
-        plt.cla()
+saisieBlock = PanedWindow(gauche, orient=HORIZONTAL, height=1)
+saisieBlock.pack(fill="both", expand="no", side=BOTTOM)
+
+saisie = Entry(saisieBlock)
+saisie.pack(fill="both", expand="yes", side=LEFT)
+
+envoyer = Button(saisieBlock, text="Envoyer")
+envoyer.pack(fill="both", expand="no", side=RIGHT)
+
+generalZone = LabelFrame(gauche, text="Vue Haut Niveau", padx=5, pady=5)
+generalZone.pack(fill="both", expand="yes")
+
+debugZone = LabelFrame(droite, text="Logs de Debug", padx=5, pady=5)
+debugZone.pack(fill="both", expand="yes", side=TOP)
+
+generalLogs = Text(generalZone, background="black", foreground="white")
+generalLogs.pack(fill="both", expand="yes")
+
+debugLogs = Text(debugZone,  background="black", foreground="white")
+debugLogs.pack(fill="both", expand="yes")
 
 
-    elif (m=="testPosition"):
-        trans=[]
-        Vt=[]
-        St=[]
-        
-        time.sleep(5)
-        while(serialCom.attendre(com)>0):
-            a=serialCom.ecouter(com)
-            data = a.rsplit("\t")
-            if len(data) == 7:
-                trans.append(np.abs(float(data[0])))
-                Vt.append(np.abs(float(data[1])))
-                St.append(np.abs(float(data[2])))
-                
-        T= np.linspace(0, 1.5, len(trans))
-        plt.plot(T, trans, 'r')
-        plt.plot(T, Vt, 'k')
-        plt.plot(T, St, 'b')
-        
-        plt.autoscale()
-        plt.show()
-        plt.cla()
-        
-    elif (m=="testRotation"):
-        trans=[]
-        Vt=[]
-        St=[]
-        
-        time.sleep(5)
-        while(serialCom.attendre(com)>0):
-            a=serialCom.ecouter(com)
-            data = a.rsplit("\t")
-            if len(data) == 7:
-                trans.append(np.abs(float(data[3])))
-                Vt.append(np.abs(float(data[4])))
-                St.append(np.abs(float(data[5])))
-                
-        T= np.linspace(0, 1.5, len(trans))
-        plt.plot(T, trans, 'r')
-        plt.plot(T, Vt, 'k')
-        plt.plot(T, St, 'b')
-        
-        plt.autoscale()
-        plt.show()
-        plt.cla()
-        
-    else:
-        time.sleep(0.1)
-        while(serialCom.attendre(com)>0):
-            a+=serialCom.ecouter(com)
+def alert():
+    showinfo("alerte", "Bravo!")
 
-        print a        
-    a=""
-    
+
+
+menubar = Menu(fenetre)
+
+menu1 = Menu(menubar, tearoff=0)
+menu1.add_command(label="Creer", command=alert)
+menu1.add_command(label="Editer", command=alert)
+menu1.add_separator()
+menu1.add_command(label="Quitter", command=fenetre.quit)
+menubar.add_cascade(label="Fichier", menu=menu1)
+
+
+menu3 = Menu(menubar, tearoff=0)
+menu3.add_command(label="A propos", command=alert)
+menubar.add_cascade(label="Aide", menu=menu3)
+
+fenetre.config(menu=menubar)
+
+
+
+bouton=Button(droite, text="Fermer", command=fenetre.quit)
+bouton.pack(side=BOTTOM)
+
+
+
+
+com=serialCom("/dev/ttyUSB0")
+
+
+
+ecoute = threadEcoute(com)
+
+ecoute.start()
 
 
 
 
 
+fenetre.mainloop()
 
+ecoute.stop()
 
     
