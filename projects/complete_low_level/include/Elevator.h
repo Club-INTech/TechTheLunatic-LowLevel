ae69ad7a4d3fd2bb972ad79794964b7908e2652e
tictacmenthe <ugurcan.ekren@telecom-sudparis.eu>
2017-01-14 00:18:16 +0100

    ça remarche, moteur sans pwm

---
+++ projects/complete_low_level/include/Elevator.h
@@ -0,0 +1,26 @@
+//
+// Created by tic-tac on 13/01/17.
+//
+
+#ifndef ARM_ELEVATOR_H
+#define ARM_ELEVATOR_H
+
+enum Sens{
+	UP, DOWN
+};
+
+class Elevator{
+private:
+	Sens sens;
+public:
+	Elevator(void);
+	void initTimer();
+	void initPWM();
+	void initPins();
+	void switchSens();
+	void setSens(Sens);
+	void run();
+	void stop();
+	void initialize();
+};
+#endif //ARM_ELEVATOR_H
