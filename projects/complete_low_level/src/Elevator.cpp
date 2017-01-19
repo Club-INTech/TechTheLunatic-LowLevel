1291ef31ba16dcdf37669337f95037e861710987
tictacmenthe <ugurcan.ekren@telecom-sudparis.eu>
2017-01-19 20:54:59 +0100

    woohoo, pwm

--- projects/complete_low_level/src/Elevator.cpp
+++ projects/complete_low_level/src/Elevator.cpp
@@ -3,8 +3,7 @@
  *
  * Pins du moteur:
  * Sens: PE9|PE11 en OUT
- * Marche: PB0 en PWM sur PA7 ?(avec un pont entre PB0 et celui là, et PB0 bloqué , TIM14)
- * (Ou PWM du TIM3ch3 sur PB0)
+ * Marche: PB0 en PWM (TIM3_CH3)
  *
  *
  * Pins de sens du moteur (bornier gauche):
@@ -26,6 +25,7 @@
 #include <stm32f4xx_tim.h>
 #include <stm32f4xx_gpio.h>
 #include <stm32f4xx_rcc.h>
+#include <stm32f4xx.h>
 #include "Elevator.h"
 
 Elevator::Elevator(void) {
@@ -39,16 +39,15 @@ void Elevator::initTimer()  //Initialise le timer
 	TIM_TimeBaseInitTypeDef timTimeBaseInitTypeDef;
 	TIM_TimeBaseStructInit(&timTimeBaseInitTypeDef);
 	
-	uint16_t prescaler = (uint16_t)((SystemCoreClock / 2) / 256000) - 1;
+	uint16_t prescaler = (uint16_t)((SystemCoreClock / 2) / 256000) - 1; //CF Motor.cpp
 	
-	//Configuration du TIMER 14
-	timTimeBaseInitTypeDef.TIM_Period=100;//ancienne valeur = 255
+	timTimeBaseInitTypeDef.TIM_Period=1000;
+	timTimeBaseInitTypeDef.TIM_ClockDivision=TIM_CKD_DIV1;
 	timTimeBaseInitTypeDef.TIM_Prescaler=prescaler;
-	timTimeBaseInitTypeDef.TIM_ClockDivision = 0;
-	timTimeBaseInitTypeDef.TIM_CounterMode = TIM_CounterMode_Up;
+	timTimeBaseInitTypeDef.TIM_CounterMode=TIM_CounterMode_Up;
+	//Configuration du TIMER 3
 	
 	TIM_TimeBaseInit(TIM3, &timTimeBaseInitTypeDef);
-
 }
 
 void Elevator::initPWM() //Initialise le PWM
@@ -56,13 +55,12 @@ void Elevator::initPWM() //Initialise le PWM
 	TIM_OCInitTypeDef timOcInitTypeDef;
 	
 	timOcInitTypeDef.TIM_OCMode=TIM_OCMode_PWM1;
-	timOcInitTypeDef.TIM_OCPolarity=TIM_OCPolarity_High;
 	timOcInitTypeDef.TIM_OutputState=TIM_OutputState_Enable;
-	timOcInitTypeDef.TIM_Pulse=100; //PWM initial nul
+	timOcInitTypeDef.TIM_OCPolarity=TIM_OCPolarity_High;
+	timOcInitTypeDef.TIM_Pulse=0; //PWM initial nul
 	
-	TIM_OC1Init(TIM3, &timOcInitTypeDef); //TIM3CH3
-	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
-	
+	TIM_OC3Init(TIM3, &timOcInitTypeDef); //Canal 2N de TIM1
+	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
 	TIM_ARRPreloadConfig(TIM3, ENABLE);
 	
 	TIM_Cmd(TIM3, ENABLE); //Active le TIM
@@ -73,45 +71,27 @@ void Elevator::initPins(void)
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE,ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE);
 	
-	GPIO_InitTypeDef gpioPinsStruct;
+	GPIO_InitTypeDef gpioPinInitStruct;
+	GPIO_StructInit(&gpioPinInitStruct);
 	
-	GPIO_StructInit(&gpioPinsStruct);
-		
-	gpioPinsStruct.GPIO_Pin=GPIO_Pin_9 + GPIO_Pin_11;
-	gpioPinsStruct.GPIO_Mode=GPIO_Mode_OUT;
-	//gpioPinEn.GPIO_Mode=GPIO_Mode_OUT;
+	gpioPinInitStruct.GPIO_Pin=GPIO_Pin_9 + GPIO_Pin_11;  //Pins de sens
+	gpioPinInitStruct.GPIO_Mode=GPIO_Mode_OUT;
+	GPIO_Init(GPIOE, &gpioPinInitStruct);
 	
+	gpioPinInitStruct.GPIO_Pin=GPIO_Pin_0;                  //Pin de pwm
+	gpioPinInitStruct.GPIO_Mode=GPIO_Mode_AF;
 	
-	GPIO_Init(GPIOE, &gpioPinsStruct);
+	gpioPinInitStruct.GPIO_PuPd=GPIO_PuPd_UP;
+	gpioPinInitStruct.GPIO_OType=GPIO_OType_PP;
+	gpioPinInitStruct.GPIO_Speed=GPIO_Speed_100MHz;
+	GPIO_Init(GPIOB, &gpioPinInitStruct);
 	
-	GPIO_StructInit(&gpioPinsStruct);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_TIM3);
 	
-	gpioPinsStruct.GPIO_Pin=GPIO_Pin_0;
-	gpioPinsStruct.GPIO_Mode=GPIO_Mode_OUT;
-	//gpioPinsStruct.GPIO_PuPd=GPIO_PuPd_UP;
-	//gpioPinsStruct.GPIO_OType=GPIO_OType_PP;
-	//gpioPinsStruct.GPIO_Speed=GPIO_Speed_100MHz;
-	
-	GPIO_Init(GPIOB, &gpioPinsStruct);
-	//GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_TIM3);
 	//Initialise le moteur dans le sens montant
 	
 	GPIO_SetBits(GPIOE, GPIO_Pin_11 + GPIO_Pin_9);
 	GPIO_ResetBits(GPIOE, GPIO_Pin_11);
-	
-//	GPIO_ResetBits(GPIOB, GPIO_Pin_0);
-
-}
-
-
-void Elevator::switchSens() { //Inverse le sens
-	if (sens==UP){
-		sens=DOWN;
-	}
-	else if (sens==DOWN){
-		sens=UP;
-	}
-	GPIO_ToggleBits(GPIOE, GPIO_Pin_11 + GPIO_Pin_9);
 }
 
 void Elevator::setSens(Sens sensToSet) { //Change la direction dans le sens souhaité(UP ou DOWN)
@@ -128,18 +108,21 @@ void Elevator::setSens(Sens sensToSet) { //Change la direction dans le sens souh
 }
 
 void Elevator::stop(void){
-	GPIO_ResetBits(GPIOB, GPIO_Pin_0);
-	//TIM3->CCR3=0;
-	switchSens();
+	TIM3->CCR3=0;
+	if(sens==UP){
+		sens=DOWN;
+	}
+	else if(sens==DOWN){
+		sens=UP;
+	}
 }
 
 void Elevator::run() {//Tourne dans le sens de sens(a déterminer empiriquement)
-	GPIO_SetBits(GPIOB, GPIO_Pin_0);
-	//TIM3->CCR3=80;
+	TIM3->CCR3=200;
 }
 
 void Elevator::initialize(void){
 	initPins();
-	//initTimer();
+	initTimer();
 	initPWM();
 }
