91b19e5f2558fcaa1cd09483760c2f46404f8a0b
Jessie <melanie.cosmides@telecom-sudparis.eu>
2017-04-19 12:25:22 +0200

    modif sens codeuses

--- projects/complete_low_level/src/Counter.cpp
+++ projects/complete_low_level/src/Counter.cpp
@@ -114,14 +114,14 @@ Counter::Counter() {
 */
 }
 
-int32_t Counter::getRightValue() {
+int32_t Counter::getLeftValue() {
 	//Translate to int32_t
-	return (-(TIM_GetCounter(TIM5)-2147483647));  //Pins codeuses inversées sur la stm
+	return ((TIM_GetCounter(TIM5)-2147483647));  //Pins codeuses inversées sur la stm
 }
 
-int32_t Counter::getLeftValue() {
+int32_t Counter::getRightValue() {
 	//Translate to int16_t
-	return (-(TIM_GetCounter(TIM3)-32767));       //Pins codeuses inversées sur la stm
+	return ((TIM_GetCounter(TIM3)-32767));       //Pins codeuses inversées sur la stm
 }
 int32_t Counter::getMoteurValue() {
     //Translate to int16_t
